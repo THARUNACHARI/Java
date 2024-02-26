5a0aad6bd816864d5bf78511039f392d8a3324c3
THARUNACHARI <tharunsarkar11@gmail.com>
2024-02-26 17:23:52 +0530

    java

---
+++ Java-One/Access Modifiers/Package_Ex2/Test.java
@@ -0,0 +1,8 @@
+public class Test
+{
+public void main(String[] args)
+{
+int X=10*10-10;
+System.out.println(x);
+}
+}
---
+++ Java-One/Interface2/Daolmpl.java
@@ -0,0 +1,20 @@
+class DaoImpl implements DaoI{
+    public void login(){
+        System.out.println("DaoImpl class login method");
+    }
+    public void logout(){
+        System.out.println("DaoImpl class logout method");
+    }
+    public boolean forgetpassword(){
+        return true;
+    }
+
+    public static void main(String[] args){
+         DaoImpl  obj=new DaoImpl();
+         obj.login();
+         obj.logout();
+         boolean result=obj.forgetpassword();
+         
+         System.out.println(result);
+    }
+}
